5f1c7d397a37724f725f738011963b7246ead311
LucasGb <lucas.macielbarros0@gmail.com>
2025-03-14 16:06:22 -0300

    terceiro commit

--- Teste/CalculadoraNota.java
+++ Teste/CalculadoraNota.java
@@ -4,19 +4,23 @@ import java.util.Scanner;
 
 public class CalculadoraNota {
     public static void main(String[] args) {
+        //Aqui será adicionado o gabarito das questoes
         char[] gabarito = {'a','b','c','d','e','a','b','c','d','e'};
         char[] resposta = new char[10];
         
         int nota = 0;
+        Scanner input = new Scanner(System.in);
         
         for (int i = 0; i < 10; i++) {
-            resposta[i] = new Scanner(System.in).next().charAt(0);
+            System.out.print("Digite a resposta da questão " + (i + 1) +": ");
+            resposta[i] = input.next().charAt(0);;
 
+            //Compara a resposta com o gabarito
             if (resposta[i] == gabarito[i]) {
                 nota++;
             }
         }
-
         System.out.printf("sua nota é: %d", nota);
+        input.close();
     }
 }
